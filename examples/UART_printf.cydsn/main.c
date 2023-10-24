85815259bc4178959ed7d92a69283e5c64e4cc21
Min Cheol Kim <kmc724@zinggls.com>
2023-10-24 10:26:06 +0900

    delete comments and add code for test

--- examples/UART_printf.cydsn/main.c
+++ examples/UART_printf.cydsn/main.c
@@ -1,26 +1,14 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include "project.h"
 
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    CyGlobalIntEnable;
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
-    for(;;)
+    UART_DBG_Start();
+    
+    while (1)
     {
-        /* Place your application code here. */
+        UART_DBG_UartPutString("Hello World\r\n");
+        CyDelay(1000);
     }
 }
-
-/* [] END OF FILE */
