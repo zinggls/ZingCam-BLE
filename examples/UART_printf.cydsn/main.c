8da734c9f93a46ade3daa84702fdcc888769318b
Min Cheol Kim <kmc724@zinggls.com>
2023-10-24 14:09:07 +0900

    add printf

--- examples/UART_printf.cydsn/main.c
+++ examples/UART_printf.cydsn/main.c
@@ -1,14 +1,31 @@
 #include "project.h"
+#include <stdio.h>
 
 int main(void)
 {
+    uint8_t cnt;
+    
     CyGlobalIntEnable;
-
     UART_DBG_Start();
+    
+    cnt = 0;
     
     while (1)
     {
-        UART_DBG_UartPutString("Hello World\r\n");
+        printf("Running cnt=%d\r\n", cnt);
+        cnt = (cnt + 1) % 0xFF;
+        
         CyDelay(1000);
     }
+}
+
+int _write(int file, char* ptr, int len)
+{
+    int i;
+    file = file;
+    for (i = 0; i < len; i++)
+    {
+        UART_DBG_UartPutChar(*ptr++);
+    }
+    return len;
 }
