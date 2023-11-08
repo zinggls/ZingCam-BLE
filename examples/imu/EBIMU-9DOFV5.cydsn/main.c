280307884900fd84edc232c6538dd10adbbff7d7
Min Cheol Kim <kmc724@zinggls.com>
2023-11-08 17:58:07 +0900

    receive imu data from EBIMU-9DoFV5

--- examples/imu/EBIMU-9DOFV5.cydsn/main.c
+++ examples/imu/EBIMU-9DOFV5.cydsn/main.c
@@ -1,14 +1,29 @@
+#include <stdio.h>
+
 #include "project.h"
 
 int main(void)
 {
+    char ch;
+    
     CyGlobalIntEnable;
     
     UART_DBG_Start();
+    UART_IMU_Start();
     
     while (1)
     {
-        UART_DBG_PutString("Hello World\r\n");
+        UART_IMU_UartPutChar('*');
+        while (1)
+        {
+            ch = UART_IMU_UartGetChar();
+            UART_DBG_PutChar(ch);
+            
+            if (ch == '\n')
+            {
+                break;
+            }
+        }
         CyDelay(1000);
     }
 }
