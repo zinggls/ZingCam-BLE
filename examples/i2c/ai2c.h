df6eae5e278e8e692a427a6e7d3a63aae80e4cee
Min Cheol Kim <kmc724@zinggls.com>
2024-02-07 11:41:22 +0900

    add i2c file in example folder

---
+++ examples/i2c/ai2c.h
@@ -0,0 +1,10 @@
+#ifndef AI2C_H
+#define AI2C_H
+
+#include "project.h"    
+    
+void AI2C_init(void);
+uint8_t AI2C_write(uint8_t addr, uint8_t* data, uint8_t len);
+uint8_t AI2C_read(uint8_t addr, uint8_t* data, uint8_t len);
+    
+#endif
