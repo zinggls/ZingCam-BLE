a069ba2ae2b3a21d1ba9404301b5aa2edf4be349
Min Cheol Kim <kmc000724@gmail.com>
2024-03-04 16:21:11 +0900

    update imu files for ICD

--- zc-ble/AIAS/ZC-BLE.cydsn/imu.c
+++ zc-ble/AIAS/ZC-BLE.cydsn/imu.c
@@ -1,4 +1,7 @@
-#include  "imu.h"
+#include "imu.h"
+#include "icd.h"
+
+static uint8_t calibrate = 0;
 
 void IMU_init(void)
 {
@@ -13,8 +16,57 @@ uint8_t IMU_get(uint16_t* values)
     uint16_t CHK;
     uint16_t DATA;
     uint8_t cnt;
+    uint8_t total_cnt;
     //char message[128];
     
+#if HBLE
+    if (calibrate == 1)
+    {
+        if (UART_IMU_UartGetChar() == '<') // <ok> or <er>
+        {
+            calibrate = 0;
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+#endif
+#if DBLE
+    if (AIAS_ICD_get(WIRELESS_VIDEO_RECEIVER_IMU_CALIBRATE) == 0x01 || // GYRO
+        AIAS_ICD_get(WIRELESS_VIDEO_RECEIVER_IMU_CALIBRATE) == 0x02 || // ACCELERO
+        AIAS_ICD_get(WIRELESS_VIDEO_RECEIVER_IMU_CALIBRATE) == 0x03)   // MAGNETO
+    {
+        if (calibrate == 0)
+        {
+            switch (AIAS_ICD_get(WIRELESS_VIDEO_RECEIVER_IMU_CALIBRATE))
+            {
+                case 0x01:
+                    IMU_calibration_gyro();
+                    break;
+                case 0x02:
+                    IMU_calibration_accelero_free();
+                    break;
+                case 0x03:
+                    IMU_calibration_magneto_free();
+                    break;
+            }
+        }
+        
+        if (UART_IMU_UartGetChar() == '<') // <ok> or <er>
+        {
+            AIAS_ICD_set(WIRELESS_VIDEO_RECEIVER_IMU_CALIBRATE, 0x80);
+            calibrate = 0;
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    AIAS_ICD_set(WIRELESS_VIDEO_RECEIVER_IMU_CALIBRATE, 0x00);
+#endif
     UART_IMU_UartPutChar('*');
     CyDelay(10);
     
@@ -22,6 +74,7 @@ uint8_t IMU_get(uint16_t* values)
     lower = UART_IMU_UartGetChar();
     SOP = (upper << 8) | lower;
     CHK = 0;
+    total_cnt = NUM_TOTAL_IMU_VALUES;
     
     if (SOP == 0x5555)
     {
@@ -35,7 +88,18 @@ uint8_t IMU_get(uint16_t* values)
             DATA = upper << 8 | lower;
             //sprintf(message, "%d. Receive %X (%X)\r\n", cnt, DATA, CHK);
             //UART_DBG_UartPutString(message);
-            if (cnt <= NUM_TOTAL_IMU_VALUES)
+#if DBLE
+            if (AIAS_ICD_get(WIRELESS_VIDEO_RECEIVER_IMU_OUTPUT_TYPE) == IMU_EULER)
+            {
+                total_cnt = NUM_TOTAL_IMU_VALUES;
+            }
+            else if (AIAS_ICD_get(WIRELESS_VIDEO_RECEIVER_IMU_OUTPUT_TYPE) == IMU_QUATERNION)
+            {
+                total_cnt = NUM_TOTAL_IMU_VALUES + 1;
+            }
+#endif
+
+            if (cnt <= total_cnt)
             {
                 values[cnt++] = DATA;
             }
@@ -51,4 +115,190 @@ uint8_t IMU_get(uint16_t* values)
     {
         return 0;
     }
+}
+
+void IMU_set_output_format(uint8_t sof)
+{
+    char msg[6];
+    
+    if (sof == IMU_EULER)
+    {
+        sprintf(msg, "<sof1>");
+#if DBLE
+        AIAS_ICD_set(WIRELESS_VIDEO_RECEIVER_IMU_OUTPUT_TYPE, 0x00);
+#endif
+    }
+    else if (sof == IMU_QUATERNION)
+    {
+        sprintf(msg, "<sof2>");
+#if DBLE
+        AIAS_ICD_set(WIRELESS_VIDEO_RECEIVER_IMU_OUTPUT_TYPE, 0x01);
+#endif
+    }
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_output_gyro(uint8_t sog)
+{
+    char msg[6];
+    
+    sprintf(msg, "<sog%d>", sog);
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_output_accelero(uint8_t soa)
+{
+    char msg[6];
+    
+    sprintf(msg, "<soa%d>", soa);
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_output_mageneto(uint8_t som)
+{
+    char msg[6];
+    
+    sprintf(msg, "<som%d>", som);
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_output_temperature(uint8_t sot)
+{
+    char msg[6];
+    
+    sprintf(msg, "<sot%d>", sot);
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_output_timestamp(uint8_t sots)
+{
+    char msg[7];
+    
+    sprintf(msg, "<sots%d>", sots);
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_calibration_gyro(void)
+{
+    char msg[4];
+    
+    calibrate = 1;
+    sprintf(msg, "<cg>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_calibration_accelero_free(void)
+{
+    char msg[5];
+    
+    calibrate = 1;
+    sprintf(msg, "<caf>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_calibration_accelero_simple(void)
+{
+    char msg[5];
+    
+    calibrate = 1;
+    sprintf(msg, "<cas>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_calibration_magneto_free(void)
+{
+    char msg[5];
+    
+    calibrate = 1;
+    sprintf(msg, "<cmf>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_calibration_magneto_xy(void)
+{
+    char msg[6];
+    
+    calibrate = 1;
+    sprintf(msg, "<cnxy>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_calibration_magneto_pxy(void)
+{
+    char msg[7];
+    
+    calibrate = 1;
+    sprintf(msg, "<+cnxy>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_calibration_magneto_z(void)
+{
+    char msg[5];
+    
+    calibrate = 1;
+    sprintf(msg, "<cnz>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_calibration_magneto_pz(void)
+{
+    char msg[6];
+    
+    calibrate = 1;
+    sprintf(msg, "<+cnz>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_motion_offset(void)
+{
+    char msg[5];
+    
+    sprintf(msg, "<cmo>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_motion_offset_x(void)
+{
+    char msg[6];
+    
+    calibrate = 1;
+    sprintf(msg, "<cmox>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_motion_offset_y(void)
+{
+    char msg[6];
+    
+    calibrate = 1;
+    sprintf(msg, "<cmoy>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_motion_offset_z(void)
+{
+    char msg[6];
+    
+    calibrate = 1;
+    sprintf(msg, "<cmoz>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_set_motion_offset_xy(void)
+{
+    char msg[7];
+    
+    calibrate = 1;
+    sprintf(msg, "<cmoxy>");
+    UART_IMU_UartPutString(msg);
+}
+
+void IMU_clear_motion_offset(void)
+{
+    char msg[6];
+    
+    calibrate = 1;
+    sprintf(msg, "<cmco>");
+    UART_IMU_UartPutString(msg);
 }
--- zc-ble/AIAS/ZC-BLE.cydsn/imu.h
+++ zc-ble/AIAS/ZC-BLE.cydsn/imu.h
@@ -5,10 +5,44 @@
 
 #include "project.h"
 
-#define NUM_TOTAL_IMU_VALUES 12
+#define NUM_TOTAL_IMU_VALUES 4
 #define MAX_IMU_VALUES_SIZE 48
 
+#define IMU_EULER 0
+#define IMU_QUATERNION 1
+    
+typedef enum
+{
+    IMU_SOF_0,
+    IMU_SOF_1,
+    IMU_SOF_2,
+    IMU_SOF_3,
+    IMU_SOTS,
+    NUM_IMU_DATAS
+} IMU_DATAS;
+    
 void IMU_init(void);
 uint8_t IMU_get(uint16_t* values);
+
+void IMU_set_output_format(uint8_t sof);
+void IMU_set_output_gyro(uint8_t sog);
+void IMU_set_output_accelero(uint8_t soa);
+void IMU_set_output_mageneto(uint8_t som);
+void IMU_set_output_temperature(uint8_t sot);
+void IMU_set_output_timestamp(uint8_t sots);
+void IMU_calibration_gyro(void);
+void IMU_calibration_accelero_free(void);
+void IMU_calibration_accelero_simple(void);
+void IMU_calibration_magneto_free(void);
+void IMU_calibration_magneto_xy(void);
+void IMU_calibration_magneto_pxy(void);
+void IMU_calibration_magneto_z(void);
+void IMU_calibration_magneto_pz(void);
+void IMU_set_motion_offset(void);
+void IMU_set_motion_offset_x(void);
+void IMU_set_motion_offset_y(void);
+void IMU_set_motion_offset_z(void);
+void IMU_set_motion_offset_xy(void);
+void IMU_clear_motion_offset(void);
 
 #endif
