3fdd1cd821976e7080d7fc99aadf91e7e25c5463
Min Cheol Kim <kmc000724@gmail.com>
2024-03-04 16:21:38 +0900

    update zing files for check I2C communication

--- zc-ble/AIAS/ZC-BLE.cydsn/zing.c
+++ zc-ble/AIAS/ZC-BLE.cydsn/zing.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 
 #include "zing.h"
+#include "icd.h"
 
 static uint8_t state = 0;
+
+static uint16_t ZING_get_device_status_dst_id_err_diff_cnt(char* dst_id_err_cnt_end_ptr);
+static uint16_t ZING_get_device_status_phy_rx_frame_diff_cnt(char* phy_rx_frame_cnt_end_ptr);
+static uint16_t ZING_get_device_status_mfir_phy_rx_frame_diff(char* dst_id_err_diff_end_ptr);
 
 uint8_t** ZING_host_init(void)
 {
@@ -214,4 +219,268 @@ void ZING_change_channel(uint8_t** host_status, uint8_t val)
             state = 0;
         }
     }
+}
+
+void ZING_set_channel_high(void)
+{
+    UART_ZING_PutChar(0x4);
+    UART_ZING_PutChar('b');
+    UART_ZING_PutChar(0x0);
+    UART_ZING_PutChar(0x0);
+}
+
+void ZING_set_channel_low(void)
+{
+    UART_ZING_PutChar(0x4);
+    UART_ZING_PutChar('b');
+    UART_ZING_PutChar(0x1);
+    UART_ZING_PutChar(0x0);
+}
+
+uint8_t ZING_get_host_status_usb(uint8_t** host_status)
+{
+    uint8_t usb = atoi((char*)host_status[HOST_STATUS_USB]);
+    
+    return usb;
+}
+
+uint16_t ZING_get_host_status_vnd(uint8_t** host_status)
+{
+    uint16_t vnd = strtol((char*)host_status[HOST_STATUS_VND], NULL, 16);
+    
+    return vnd;
+}
+
+uint16_t ZING_get_host_status_prd(uint8_t** host_status)
+{
+    uint16_t prd = strtol((char*)host_status[HOST_STATUS_PRD], NULL, 16);
+    
+    return prd;
+}
+
+char ZING_get_host_status_bnd(uint8_t** host_status)
+{
+    char bnd = *host_status[HOST_STATUS_BND];
+    
+    return bnd;
+}
+
+uint16_t ZING_get_host_status_ppid(uint8_t** host_status)
+{
+    uint16_t ppid = strtol((char*)host_status[HOST_STATUS_PPID], NULL, 16);
+    
+    return ppid;
+}
+
+uint16_t ZING_get_host_status_devid(uint8_t** host_status)
+{
+    uint16_t devid = strtol((char*)host_status[HOST_STATUS_DEVID], NULL, 16);
+    
+    return devid;
+}
+
+uint8_t ZING_get_host_status_fmt(uint8_t** host_status)
+{
+    uint8_t fmt = atoi((char*)host_status[HOST_STATUS_FMT]);
+    
+    return fmt;
+}
+
+uint8_t ZING_get_host_status_idx(uint8_t** host_status)
+{
+    uint8_t idx = atoi((char*)host_status[HOST_STATUS_IDX]);
+    
+    return idx;
+}
+
+char ZING_get_host_status_trt(uint8_t** host_status)
+{
+    char trt = *host_status[HOST_STATUS_TRT];
+    
+    return trt;
+}
+
+char ZING_get_host_status_ack(uint8_t** host_status)
+{
+    char ack = *host_status[HOST_STATUS_ACK];
+    
+    return ack;
+}
+
+char ZING_get_host_status_ppc(uint8_t** host_status)
+{
+    char ppc = *host_status[HOST_STATUS_PPC];
+    
+    return ppc;
+}
+
+char ZING_get_host_status_run(uint8_t** host_status)
+{
+    char run = *host_status[HOST_STATUS_RUN];
+    
+    return run;
+}
+
+uint32_t ZING_get_host_status_txid(uint8_t** host_status)
+{
+    uint32_t txid = strtol((char*)host_status[HOST_STATUS_TXID], NULL, 16);
+    
+    return txid;
+}
+
+uint32_t ZING_get_host_status_rxid(uint8_t** host_status)
+{
+    uint32_t rxid = strtol((char*)host_status[HOST_STATUS_RXID], NULL, 16);
+    
+    return rxid;
+}
+
+uint32_t ZING_get_host_status_cnt(uint8_t** host_status)
+{
+    uint32_t cnt = atoi((char*)host_status[HOST_STATUS_CNT]);
+    
+    return cnt;
+}
+
+uint8_t ZING_get_device_status_usb(uint8_t** device_status)
+{
+    uint8_t usb = atoi((char*)device_status[DEVICE_STATUS_USB]);
+    
+    return usb;
+}
+
+uint16_t ZING_get_device_status_ppid(uint8_t** device_status)
+{
+    uint16_t ppid = strtol((char*)device_status[DEVICE_STATUS_PPID], NULL, 16);
+    
+    return ppid;
+}
+
+uint16_t ZING_get_device_status_devid(uint8_t** device_status)
+{
+    uint16_t devid = strtol((char*)device_status[DEVICE_STATUS_DEVID], NULL, 16);
+    
+    return devid;
+}
+
+uint8_t ZING_get_device_status_fmt(uint8_t** device_status)
+{
+    uint8_t fmt = atoi((char*)device_status[DEVICE_STATUS_FMT]);
+    
+    return fmt;
+}
+
+uint8_t ZING_get_device_status_idx(uint8_t** device_status)
+{
+    uint8_t idx = atoi((char*)device_status[DEVICE_STATUS_IDX]);
+    
+    return idx;
+}
+
+char ZING_get_device_status_trt(uint8_t** device_status)
+{
+    char trt = *device_status[DEVICE_STATUS_TRT];
+    
+    return trt;
+}
+
+char ZING_get_device_status_ack(uint8_t** device_status)
+{
+    char ack = *device_status[DEVICE_STATUS_ACK];
+    
+    return ack;
+}
+
+char ZING_get_device_status_ppc(uint8_t** device_status)
+{
+    char ppc = *device_status[DEVICE_STATUS_PPC];
+    
+    return ppc;
+}
+
+
+char ZING_get_device_status_run(uint8_t** device_status)
+{
+    char run = *device_status[DEVICE_STATUS_RUN];
+    
+    return run;
+}
+
+char ZING_get_device_status_itf(uint8_t** device_status)
+{
+    char run = *device_status[DEVICE_STATUS_ITF];
+    
+    return run;
+}
+
+uint32_t ZING_get_device_status_txid(uint8_t** device_status)
+{
+    uint32_t txid = strtol((char*)device_status[DEVICE_STATUS_TXID], NULL, 16);
+    
+    return txid;
+}
+
+uint32_t ZING_get_device_status_rxid(uint8_t** device_status)
+{
+    uint32_t rxid = strtol((char*)device_status[DEVICE_STATUS_RXID], NULL, 16);
+    
+    return rxid;
+}
+
+ZING_diff ZING_get_device_status_dst_id_err_cnt(uint8_t** device_status)
+{
+    ZING_diff dst_id_err;
+    char* endptr;
+    dst_id_err.cnt = strtol((char*)device_status[DEVICE_STATUS_DST_ID_ERR_CNT], &endptr, 10);
+    dst_id_err.diff = ZING_get_device_status_dst_id_err_diff_cnt(endptr);
+    
+    return dst_id_err;
+}
+
+uint16_t ZING_get_device_status_dst_id_err_diff_cnt(char* dst_id_err_cnt_end_ptr)
+{
+    uint16_t dst_id_err_diff_cnt = strtol(dst_id_err_cnt_end_ptr + 1, NULL, 10);
+    
+    return dst_id_err_diff_cnt;
+}
+
+ZING_diff ZING_get_device_status_phy_rx_frame_cnt(uint8_t** device_status)
+{
+    ZING_diff phy_rx_frame;
+    char* endptr;
+    phy_rx_frame.cnt = strtol((char*)device_status[DEVICE_STATUS_PHY_RX_FRAME_CNT], &endptr, 10);
+    phy_rx_frame.diff = ZING_get_device_status_phy_rx_frame_diff_cnt(endptr);
+    
+    return phy_rx_frame;
+}
+
+uint16_t ZING_get_device_status_phy_rx_frame_diff_cnt(char* phy_rx_frame_cnt_end_ptr)
+{
+    uint16_t phy_rx_frame_diff_cnt = strtol(phy_rx_frame_cnt_end_ptr + 1, NULL, 10);
+    
+    return phy_rx_frame_diff_cnt;
+}
+
+ZING_mfir ZING_get_device_status_mfir(uint8_t** device_status)
+{
+    ZING_mfir mfir;
+    char* endptr;
+    mfir.dst_id_err_diff = strtol((char*)device_status[DEVICE_STATUS_MFIR], &endptr, 10);
+    mfir.phy_rx_frame_diff = ZING_get_device_status_mfir_phy_rx_frame_diff(endptr);
+    
+    return mfir;
+}
+
+uint16_t ZING_get_device_status_mfir_phy_rx_frame_diff(char* mfir_dst_id_err_diff_end_ptr)
+{
+    uint16_t mfir_phy_rx_frame_diff = strtol(mfir_dst_id_err_diff_end_ptr + 1, NULL, 10);
+    
+    return mfir_phy_rx_frame_diff;
+}
+
+uint32_t ZING_get_device_status_cnt(uint8_t** device_status)
+{
+    uint32_t cnt = atoi((char*)device_status[DEVICE_STATUS_CNT]);
+    
+    return cnt;
 }
--- zc-ble/AIAS/ZC-BLE.cydsn/zing.h
+++ zc-ble/AIAS/ZC-BLE.cydsn/zing.h
@@ -50,7 +50,19 @@ typedef enum {
     DEVICE_STATUS_CNT,
     NUM_DEVICE_STATUS
 } ZING_DEVICE_STATUS_NAME;
-    
+
+typedef struct
+{
+    uint32_t cnt;
+    uint16_t diff;
+} ZING_diff;
+
+typedef struct
+{
+    uint16_t dst_id_err_diff;
+    uint16_t phy_rx_frame_diff;
+} ZING_mfir; 
+
 uint8_t** ZING_host_init(void);
 uint8_t** ZING_device_init(void);
 uint8_t ZING_get_host_status(char* data);
@@ -58,5 +70,39 @@ uint8_t ZING_get_device_status(char* data);
 uint8_t ZING_parse_host_status(char* device_status, uint8_t** status_values);
 uint8_t ZING_parse_device_status(char* device_status, uint8_t** status_values);
 void ZING_change_channel(uint8_t** host_status, uint8_t val);
+void ZING_set_channel_high(void);
+void ZING_set_channel_low(void);
+
+uint8_t ZING_get_host_status_usb(uint8_t** host_status);
+uint16_t ZING_get_host_status_vnd(uint8_t** host_status);
+uint16_t ZING_get_host_status_prd(uint8_t** host_status);
+char ZING_get_host_status_bnd(uint8_t** host_status);
+uint16_t ZING_get_host_status_ppid(uint8_t** host_status);
+uint16_t ZING_get_host_status_devid(uint8_t** host_status);
+uint8_t ZING_get_host_status_fmt(uint8_t** host_status);
+uint8_t ZING_get_host_status_idx(uint8_t** host_status);
+char ZING_get_host_status_trt(uint8_t** host_status);
+char ZING_get_host_status_ack(uint8_t** host_status);
+char ZING_get_host_status_ppc(uint8_t** host_status);
+uint32_t ZING_get_host_status_txid(uint8_t** host_status);
+uint32_t ZING_get_host_status_rxid(uint8_t** host_status);
+char ZING_get_host_status_run(uint8_t** host_status);
+uint32_t ZING_get_host_status_cnt(uint8_t** host_status);
+uint8_t ZING_get_device_status_usb(uint8_t** device_status);
+uint16_t ZING_get_device_status_ppid(uint8_t** device_status);
+uint16_t ZING_get_device_status_devid(uint8_t** device_status);
+uint8_t ZING_get_device_status_fmt(uint8_t** device_status);
+uint8_t ZING_get_device_status_idx(uint8_t** device_status);
+char ZING_get_device_status_trt(uint8_t** device_status);
+char ZING_get_device_status_ack(uint8_t** device_status);
+char ZING_get_device_status_ppc(uint8_t** device_status);
+char ZING_get_device_status_run(uint8_t** device_status);
+char ZING_get_device_status_itf(uint8_t** device_status);
+uint32_t ZING_get_device_status_txid(uint8_t** device_status);
+uint32_t ZING_get_device_status_rxid(uint8_t** device_status);
+ZING_diff ZING_get_device_status_dst_id_err_cnt(uint8_t** device_status);
+ZING_diff ZING_get_device_status_phy_rx_frame_cnt(uint8_t** device_status);
+ZING_mfir ZING_get_device_status_mfir(uint8_t** device_status);
+uint32_t ZING_get_device_status_cnt(uint8_t** device_status);
 
 #endif
