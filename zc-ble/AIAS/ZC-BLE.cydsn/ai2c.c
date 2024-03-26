88286a1ef82b25f96da84ae5d60393e33496c286
Min Cheol Kim <kmc000724@gmail.com>
2024-03-26 14:10:15 +0900

    AIAS ICD에 맞게 파라미터 변경

--- zc-ble/AIAS/ZC-BLE.cydsn/ai2c.c
+++ zc-ble/AIAS/ZC-BLE.cydsn/ai2c.c
@@ -1,9 +1,10 @@
 #include "ai2c.h"
 #include "main.h"
+#include "icd.h"
 
 #if DBLE
-#define I2C_WR_BUFFER_SIZE 11
-#define I2C_RD_BUFFER_SIZE 40 + 16
+#define I2C_WR_BUFFER_SIZE NUM_WRITE_AIAS_ICD
+#define I2C_RD_BUFFER_SIZE NUM_READ_AIAS_ICD
     
 static uint8_t i2c_slave_wr_buffer[I2C_WR_BUFFER_SIZE];
 static uint8_t i2c_slave_rd_buffer[I2C_RD_BUFFER_SIZE];
@@ -22,7 +23,7 @@ void AI2C_init(void)
 uint8_t AI2C_write(uint8_t addr, uint8_t* data, uint8_t len)
 #endif
 #if DBLE
-uint8_t AI2C_write(uint8_t* data, uint8_t len)
+uint8_t AI2C_write(void)
 #endif
 {
     uint8_t status;
@@ -52,19 +53,15 @@ uint8_t AI2C_write(uint8_t* data, uint8_t len)
     
 #endif
 #if DBLE
-    // I2C SLAVE's read is TX
-    
-    memcpy(i2c_slave_rd_buffer, data, len);
-        
-    if (((status = I2C_I2CSlaveStatus()) & I2C_I2C_SSTAT_RD_CMPLT) != 0)
-    {
-        if (I2C_I2CSlaveGetReadBufSize() == len)
+    if (((status = I2C_I2CSlaveStatus()) & I2C_I2C_SSTAT_WR_CMPLT) != 0)
+    {    
+        if (I2C_I2CSlaveGetWriteBufSize() == I2C_WR_BUFFER_SIZE)
         {
             
         }
         
-        I2C_I2CSlaveClearReadBuf();
-        I2C_I2CSlaveClearReadStatus();
+        I2C_I2CSlaveClearWriteBuf();
+        I2C_I2CSlaveClearWriteStatus();
     }
     else
     {
@@ -78,7 +75,7 @@ uint8_t AI2C_write(uint8_t* data, uint8_t len)
 uint8_t AI2C_read(uint8_t addr, uint8_t* data, uint8_t len)
 #endif
 #if DBLE
-uint8_t AI2C_read(uint8_t* data, uint8_t len)
+uint8_t AI2C_read(void)
 #endif
 {
     uint8_t status;
@@ -107,16 +104,15 @@ uint8_t AI2C_read(uint8_t* data, uint8_t len)
     }
 #endif
 #if DBLE
-    // I2C SLAVE's write is RX
-    if (((status = I2C_I2CSlaveStatus()) & I2C_I2C_SSTAT_WR_CMPLT) != 0)
-    {    
-        if (I2C_I2CSlaveGetWriteBufSize() == len)
+    if (((status = I2C_I2CSlaveStatus()) & I2C_I2C_SSTAT_RD_CMPLT) != 0)
+    {
+        if (I2C_I2CSlaveGetReadBufSize() == I2C_RD_BUFFER_SIZE)
         {
-            memcpy(data, i2c_slave_wr_buffer, len);
+            
         }
         
-        I2C_I2CSlaveClearWriteBuf();
-        I2C_I2CSlaveClearWriteStatus();
+        I2C_I2CSlaveClearReadBuf();
+        I2C_I2CSlaveClearReadStatus();
     }
     else
     {
@@ -125,3 +121,15 @@ uint8_t AI2C_read(uint8_t* data, uint8_t len)
 #endif
     return 0;
 }
+
+#if DBLE
+void AI2C_set_read_buffer(uint8_t* buffer, uint8_t len)
+{
+    memcpy(i2c_slave_rd_buffer, buffer, len);
+}
+
+void AI2C_set_write_buffer(uint8_t* buffer, uint8_t len)
+{
+    memcpy(buffer, i2c_slave_wr_buffer, len);
+}
+#endif
