0bbabd73a3c679320800f864ae9f8c1d968dd21f
Min Cheol Kim <kmc000724@gmail.com>
2024-05-21 13:19:48 +0900

    AIAS ICd 파일 추가

---
+++ zc-ble/IE/aias.py
@@ -0,0 +1,107 @@
+scope_camera = {
+    0 : "Default",
+    1 : "EO",
+    2 : "IR",
+}
+
+scope_output = {
+    0 : "출력",
+    1 : "미출력",
+}
+
+wireless_video_channel_mode = {
+    1 : "자동",
+    2 : "수동",
+}
+
+wireless_video_channel_information = {
+    0 : "자동",
+    1 : "수동 1채널",
+    2 : "수동 2채널",
+}
+
+scope_operation_mode = {
+    0 : "Power off",
+    1 : "운용모드",
+    2 : "대기모드",
+    4 : "절전모드",
+    0xFF : "Unknown",
+}
+
+wireless_video_transmitter_operation_mode_status = {
+    0 : "Booting",
+    1 : "운용모드",
+    2 : "대기모드",
+    4 : "절전모드",
+    0xFF : "Unknown",
+}
+
+wireless_video_receiver_operation_mode_status = {
+    1 : "운용모드",
+    2 : "대기모드",
+    4 : "절전모드",
+}
+
+wireless_video_transmitter_imu_output_type = {
+    0 : "Euler Angle",
+    1 : "Quaternion",
+}
+
+wireless_video_transmitter_imu_calibrate = {
+    0 : "Default",
+    1 : "보정 시작",
+    2 : "보정 종료",
+}
+
+wireless_video_receiver_imu_output_type = {
+    0 : "Euler Angle",
+    1 : "Quaternion",
+}
+
+wireless_video_receiver_imu_calibrate = {
+    0 : "Default",
+    1 : "보정 시작",
+    2 : "보정 종료",
+}
+
+scope_ir_module_status = {
+    0 : "Default",
+    0xE1 : "화기조준경 IR 모듈 에러",
+    0xFF : "Unknown",
+}
+
+scope_eo_module_status = {
+    0 : "Default",
+    0xE2 : "화기조준경 EO 모듈 에러",
+    0xFF : "Unknown",
+}
+
+wireless_video_transmitter_modem_status = {
+    0 : "Default",
+    0xE3 : "무선영상 송신기 모뎀 이상",
+    0xFF : "Unknown",
+}
+
+wireless_video_receiver_modem_status = {
+    0 : "Default",
+    0xE4 : "무선영상 수신기 모뎀 이상",
+    0xFF : "Unknown",
+}
+
+wireless_video_transmitter_imu_status = {
+    0 : "Default",
+    0xE5 : "무선영상 송신기 IMU 이상",
+    0xFF : "Unknown",
+}
+
+wireless_video_receiver_imu_status = {
+    0 : "Default",
+    0xE6 : "무선영상 수신기 IMU 이상",
+    0xFF : "Unknown",
+}
+
+aias_ble_status = {
+    0 : "대기",
+    1 : "페어링 중",
+    2 : "페어링 완료",
+}
