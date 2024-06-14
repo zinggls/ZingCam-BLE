1c58914accf4d61ffd4abd62955bf6538bc14cc3
Min Cheol Kim <kmc000724@gmail.com>
2024-06-14 18:05:23 +0900

    ZING 채널 밴드 값 변경

--- zc-ble/AIAS/ZC-BLE.cydsn/zing.c
+++ zc-ble/AIAS/ZC-BLE.cydsn/zing.c
@@ -247,7 +247,7 @@ uint8_t ZING_get_ZED(void)
 void ZING_set_channel_high(void)
 {
 #if HBLE
-    uint8_t arr[4] = { 0x4, 'b', 0x0, 0x0 };
+    uint8_t arr[4] = { 0x4, 'b', 0x1, 0x0 };
     
     if (current_channel == ZING_INFO_CH1)
     {
@@ -266,7 +266,7 @@ void ZING_set_channel_high(void)
 void ZING_set_channel_low(void)
 {
 #if HBLE
-    uint8_t arr[4] = { 0x4, 'b', 0x1, 0x0 };
+    uint8_t arr[4] = { 0x4, 'b', 0x0, 0x0 };
     
     if (current_channel == ZING_INFO_CH2)
     {
