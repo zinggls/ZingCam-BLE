14128c933d6ef912e0731ddf868a012204a018ad
Min Cheol Kim <kmc000724@gmail.com>
2024-07-29 13:55:24 +0900

    issue #1297, 오일러 기준이었던 코드를 쿼터니안도 동작시킬 수 있도록 수정

--- zc-ble/AIAS/ZC-BLE.cydsn/imu.c
+++ zc-ble/AIAS/ZC-BLE.cydsn/imu.c
@@ -3,7 +3,7 @@
 
 static uint8_t type = IMU_EULER;
 static uint8_t calibrate = 0;
-static uint8_t imu_status[NUM_IMU_DATA_BYTES + 1];
+static uint8_t imu_status[NUM_IMU_QUATERNION_DATA_BYTES + 1];
 static uint8_t imu_cnt = 0;
 static uint16_t checksum = 0;
 static uint8_t SOF = 0;
@@ -14,31 +14,10 @@ static uint8_t check_ok = 0;
 void UART_IMU_RX_INTERRUPT(void)
 {
     char ch;
+    uint8_t num_bytes;
     
     ch = UART_IMU_UartGetByte();
-    
-    if (calibrate == 1)
-    {
-        if (ch == '<')
-        {            
-            memset(imu_status, 0, NUM_IMU_DATA_BYTES + 1);
-            imu_cnt = 0;
-        }
-        
-        if (ch == '>')
-        {
-            if (check_ok != 2)
-            {
-                check_ok = check_ok + 1;
-            }
-            else
-            {
-                calibrate = 0;
-            }
-        }
-        
-        return;
-    }
+    num_bytes = (type == 0) ? NUM_IMU_EULER_DATA_BYTES : NUM_IMU_QUATERNION_DATA_BYTES;
     
     if (ch == 0x55)
     {
@@ -63,18 +42,18 @@ void UART_IMU_RX_INTERRUPT(void)
     }
     else
     {
-        if (imu_cnt < NUM_IMU_DATA_BYTES)
+        if (imu_cnt < num_bytes)
         {
             imu_status[imu_cnt] = ch;
             checksum = checksum + ch;
         }
         else
         {
-            if (imu_cnt == NUM_IMU_CHECKSUM_UPPER)
+            if (imu_cnt == num_bytes)
             {
                 checksum_upper = ch;
             }
-            else if (imu_cnt == NUM_IMU_CHECKSUM_LOWER)
+            else if (imu_cnt == num_bytes + 1)
             {
                 checksum_lower = ch;
                 
@@ -84,12 +63,12 @@ void UART_IMU_RX_INTERRUPT(void)
                 if (checksum == ((checksum_upper << 8) | checksum_lower))
                 {
                     // IMU SUCCESS
-                    imu_status[NUM_IMU_DATA_BYTES] = 1;
+                    imu_status[num_bytes] = 1;
                 }
                 else
                 {
                     // IMU ERROR
-                    imu_status[NUM_IMU_DATA_BYTES] = 0;
+                    imu_status[num_bytes] = 0;
                 }
                 checksum = 0;
             }
@@ -100,7 +79,7 @@ void UART_IMU_RX_INTERRUPT(void)
                 SOF = 0;
                 
                 // IMU ERROR
-                imu_status[NUM_IMU_DATA_BYTES] = 0;
+                imu_status[num_bytes] = 0;
             }
         }
     }
@@ -121,30 +100,24 @@ uint8_t IMU_get(uint16_t* values)
 {
     uint8_t upper;
     uint8_t lower;
+    uint8_t num_bytes;
 
-    UART_IMU_UartPutChar('*');
+    num_bytes = (type == 0) ? NUM_IMU_EULER_DATA_BYTES : NUM_IMU_QUATERNION_DATA_BYTES;
     
-    if (calibrate == 1)
+    if (imu_status[num_bytes] == 0) // checksum failed
     {
-        return 0;
+        return 0; // this is not imu's problem
     }
-    else
-    {   
-        if (imu_status[NUM_IMU_DATA_BYTES] == 0) // checksum failed
-        {
-            return 0; // this is not imu's problem
-        }
+    
+    for (uint8_t i = 2; i < num_bytes; i = i + 2)
+    {
+        upper = imu_status[i];
+        lower = imu_status[i + 1];
         
-        for (uint8_t i = 2; i < NUM_IMU_DATA_BYTES; i = i + 2)
-        {
-            upper = imu_status[i];
-            lower = imu_status[i + 1];
-            
-            values[(i / 2) - 1] = ((upper << 8) | lower);
-        }
-        
-        return 1;
+        values[(i / 2) - 1] = ((upper << 8) | lower);
     }
+    
+    return 1;
 }
 
 uint8_t IMU_get_type(void)
@@ -163,13 +136,13 @@ void IMU_set_output_format(uint8_t sof)
     
     if (sof == IMU_EULER)
     {
-        sprintf(msg, "<sof1>");
         type = IMU_EULER;
+        sprintf(msg, "<sof1>");
     }
     else if (sof == IMU_QUATERNION)
     {
-        sprintf(msg, "<sof2>");
         type = IMU_QUATERNION;
+        sprintf(msg, "<sof2>");
     }
     UART_IMU_UartPutString(msg);
 }
@@ -218,166 +191,122 @@ void IMU_calibration_gyro(void)
 {
     char msg[4];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cg>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<cg>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_calibration_accelero_free(void)
 {
     char msg[5];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<caf>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<caf>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_calibration_accelero_simple(void)
 {
     char msg[5];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cas>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<cas>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_calibration_magneto_free(void)
 {
     char msg[5];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cmf>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<cmf>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_calibration_magneto_xy(void)
 {
     char msg[6];
-    
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cnxy>");
-        UART_IMU_UartPutString(msg);
-    }
+
+    calibrate = 1;
+    sprintf(msg, "<cnxy>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_calibration_magneto_pxy(void)
 {
     char msg[7];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<+cnxy>");
-        UART_IMU_UartPutString(msg);
-    }
+    sprintf(msg, "<+cnxy>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_calibration_magneto_z(void)
 {
     char msg[5];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cnz>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<cnz>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_calibration_magneto_pz(void)
 {
     char msg[6];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<+cnz>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<+cnz>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_set_motion_offset(void)
 {
     char msg[5];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cmo>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<cmo>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_set_motion_offset_x(void)
 {
     char msg[6];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cmox>");
-        UART_IMU_UartPutString(msg);
-    }
+    sprintf(msg, "<cmox>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_set_motion_offset_y(void)
 {
     char msg[6];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cmoy>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<cmoy>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_set_motion_offset_z(void)
 {
     char msg[6];
-    
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cmoz>");
-        UART_IMU_UartPutString(msg);
-    }
+
+    calibrate = 1;
+    sprintf(msg, "<cmoz>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_set_motion_offset_xy(void)
 {
     char msg[7];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cmoxy>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<cmoxy>");
+    UART_IMU_UartPutString(msg);
 }
 
 void IMU_clear_motion_offset(void)
 {
     char msg[6];
     
-    if (calibrate != 1)
-    {
-        calibrate = 1;
-        sprintf(msg, "<cmco>");
-        UART_IMU_UartPutString(msg);
-    }
+    calibrate = 1;
+    sprintf(msg, "<cmco>");
+    UART_IMU_UartPutString(msg);
 }
