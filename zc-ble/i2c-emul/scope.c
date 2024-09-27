0cf755153f1c467ba49045e46728d93c932fe36d
Rossi Kwak <rossi.kwak@gmail.com>
2024-09-27 13:39:42 +0900

    define Scope type

--- zc-ble/i2c-emul/scope.c
+++ zc-ble/i2c-emul/scope.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-    
+#include <stdint.h>
+
+typedef struct {
+	uint8_t camera;
+	uint8_t output;
+	uint8_t mode;
+	uint8_t battery_status;
+	uint8_t ir_status;
+	uint8_t eo_status;
+} Scope;
+
 int main(int argc, char *argv[])
 {  
 	printf("I2C master emulating the scope\r\n");
