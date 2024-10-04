4c4848c1876fb71b37b69b614a652e3506d9d818
Rossi Kwak <rossi.kwak@gmail.com>
2024-10-04 21:20:09 +0900

    I2C클래스를 별도의 파일로 분리

---
+++ zc-ble/IE/I2C.py
@@ -0,0 +1,36 @@
+import win32com.client
+
+class I2C:
+    def __init__(self):
+        self.dev = win32com.client.Dispatch("PSoCProgrammerCOM.PSoCProgrammerCOM_Object")
+
+    def get_ports(self):
+        result = self.dev.GetPorts()
+        ports = result[1]
+        return ports
+    
+    def open_port(self, port):
+        result = self.dev.OpenPort(port)
+        return result
+    
+    def close_port(self):
+        result = self.dev.ClosePort()
+        return result
+    
+    def get_address(self):
+        try:
+            result = self.dev.I2C_GetDeviceList()
+            address = result[1][0]
+        except:
+            self.close_port()
+            exit()
+        return address
+    
+    def read_use_addr(self, device_addr):
+        result = self.dev.I2C_ReadData(device_addr, 44 + 26 + 42)
+        read_data = result[1]
+        return read_data
+    
+    def send_use_addr(self, device_addr, dataIN):
+        data = array.array('B', dataIN)
+        self.dev.I2C_SendData(device_addr, memoryview(data))
--- zc-ble/IE/ie.py
+++ zc-ble/IE/ie.py
@@ -1,4 +1,3 @@
-import win32com.client
 import tkinter
 import tkinter.ttk
 import array
@@ -6,41 +5,7 @@ import em2890
 import subprocess
 import zingcam
 import aias
-
-class I2C:
-    def __init__(self):
-        self.dev = win32com.client.Dispatch("PSoCProgrammerCOM.PSoCProgrammerCOM_Object")
-
-    def get_ports(self):
-        result = self.dev.GetPorts()
-        ports = result[1]
-        return ports
-    
-    def open_port(self, port):
-        result = self.dev.OpenPort(port)
-        return result
-    
-    def close_port(self):
-        result = self.dev.ClosePort()
-        return result
-    
-    def get_address(self):
-        try:
-            result = self.dev.I2C_GetDeviceList()
-            address = result[1][0]
-        except:
-            self.close_port()
-            exit()
-        return address
-    
-    def read_use_addr(self, device_addr):
-        result = self.dev.I2C_ReadData(device_addr, 44 + 26 + 42)
-        read_data = result[1]
-        return read_data
-    
-    def send_use_addr(self, device_addr, dataIN):
-        data = array.array('B', dataIN)
-        self.dev.I2C_SendData(device_addr, memoryview(data))
+import I2C
     
 class ICD:
     def __init__(self):
@@ -975,5 +940,5 @@ class IE:
                     self.run = subprocess.Popen(command)
 
 
-i2c = I2C()
+i2c = I2C.I2C()
 ie = IE(i2c)
