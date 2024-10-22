a96333bf41b06c299ddbec38905736c37c243392
Rossi Kwak <rossi.kwak@gmail.com>
2024-10-22 16:46:38 +0900

    Zing에서 보내는 로그를 UART_ZING에서 수신하고 이를 UART_DBG를 통해 출력

--- examples/examples/zing.cydsn/main.c
+++ examples/examples/zing.cydsn/main.c
@@ -17,12 +17,13 @@ int main(void)
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     UART_DBG_Start();
+    UART_ZING_Start();
 
+    UART_DBG_UartPutString("Start\r\n");
     for(;;)
     {
         /* Place your application code here. */
-        UART_DBG_UartPutString("Hello Uart\r\n");
-        CyDelay(1000);
+        UART_DBG_UartPutChar(UART_ZING_GetChar());
     }
 }
 
