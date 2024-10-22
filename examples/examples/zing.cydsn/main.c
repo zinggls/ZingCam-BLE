e123d896a3b022c28cd0aacff51a09bb46d04807
Rossi Kwak <rossi.kwak@gmail.com>
2024-10-22 21:45:07 +0900

    UART_ZING_RX_INTR 추가

--- examples/examples/zing.cydsn/main.c
+++ examples/examples/zing.cydsn/main.c
@@ -11,6 +11,11 @@
 */
 #include "project.h"
 
+CY_ISR(UART_ZING_RX_INTERRUPT)
+{   
+    return;
+}
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
@@ -18,6 +23,7 @@ int main(void)
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     UART_DBG_Start();
     UART_ZING_Start();
+    UART_ZING_RX_INTR_StartEx(UART_ZING_RX_INTERRUPT);
 
     UART_DBG_UartPutString("Start\r\n");
     for(;;)
