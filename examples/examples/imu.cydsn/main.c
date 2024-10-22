c5019d25fa3ee6a1168d7d3346fec68aab73c078
Rossi Kwak <rossi.kwak@gmail.com>
2024-10-22 15:48:41 +0900

    Hex 출력 모드로 IMU센서 값 읽어오기 <soc2> : Hex 출력 모드

--- examples/examples/imu.cydsn/main.c
+++ examples/examples/imu.cydsn/main.c
@@ -10,18 +10,31 @@
  * ========================================
 */
 #include "project.h"
+#include <stdio.h>
 
 int main(void)
 {
+    uint8_t lower,upper;
+    uint16_t word;
+    char msg[128];
+
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     UART_DBG_Start();
+    UART_IMU_Start();
+
+    UART_IMU_UartPutString("<soc2>");
 
     for(;;)
     {
         /* Place your application code here. */
-        UART_DBG_UartPutString("Hello Uart\r\n");
+        upper = UART_IMU_UartGetChar();
+        lower = UART_IMU_UartGetChar();
+        word = upper <<8 | lower;
+
+        sprintf(msg, "%X\r\n", word);
+        UART_DBG_UartPutString(msg);
         CyDelay(1000);
     }
 }
