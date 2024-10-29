b040df46bbb2cd03edc7644cb615e9b9e9819941
Rossi Kwak <rossi.kwak@gmail.com>
2024-10-29 14:35:43 +0900

    zcd 로그 파싱 구현 workspace업데이트
    * ZCD입력 로그 예
    ZCD USB:2 PPID:0xABCD DeviceID:0x3500 FMT:1 IDX:1 FPS:0x51615 TRT:B ACK:N PPC:D RUN:N ITF:N TXID:0x0 RXID:0x35ABCD DestID_ERR_CNT:5(0) PHY_RX_FRAME_CNT:8034854(228) MFIR:0/228 CNT:48
    ZCD USB:2 PPID:0xABCD DeviceID:0x3500 FMT:1 IDX:1 FPS:0x51615 TRT:B ACK:N PPC:D RUN:N ITF:N TXID:0x0 RXID:0x35ABCD DestID_ERR_CNT:5(0) PHY_RX_FRAME_CNT:8035082(228) MFIR:0/228 CNT:48
    
    * zing-zcd에서 파싱한 결과 출력 로그 예
    ZCD USB:2 PPID:0xABCD DeviceID:0x3500 FMT:1 IDX:1 FPS:0x51615 TRT:B ACK:N PPC:D RUN:N ITF:N TXID:0x0 RXID:0x35ABCD DestID_ERR_CNT:5(0) PHY_RX_FRAME_CNT:8537425(234) MFIR:0/234 CNT:48
    ZCD USB:2 PPID:0xABCD DeviceID:0x3500 FMT:1 IDX:1 FPS:0x51615 TRT:B ACK:N PPC:D RUN:N ITF:N TXID:0x0 RXID:0x35ABCD DestID_ERR_CNT:5(0) PHY_RX_FRAME_CNT:8537653(228) MFIR:0/228 CNT:48
    ZCD USB:2 PPID:0xABCD DeviceID:0x3500 FMT:1 IDX:1 FPS:0x51615 TRT:B ACK:N PPC:D RUN:N ITF:N TXID:0x0 RXID:0x35ABCD DestID_ERR_CNT:5(0) PHY_RX_FRAME_CNT:8537881(228) MFIR:0/228 CNT:48
    Parsing Error
    Parsing Error
    ZCD USB:2 PPID:0xABCD DeviceID:0x3500 FMT:1 IDX:1 FPS:0x51615 TRT:B ACK:N PPC:D RUN:N ITF:N TXID:0x0 RXID:0x35ABCD DestID_ERR_CNT:5(0) PHY_RX_FRAME_CNT:8538571(231) MFIR:0/231 CNT:48
    ZCD USB:2 PPID:0xABCD DeviceID:0x3500 FMT:1 IDX:1 FPS:0x51615 TRT:B ACK:N PPC:D RUN:N ITF:N TXID:0x0 RXID:0x35ABCD DestID_ERR_CNT:5(0) PHY_RX_FRAME_CNT:8538802(231) MFIR:0/231 CNT:48

--- examples/examples/zing-zcd.cydsn/main.c
+++ examples/examples/zing-zcd.cydsn/main.c
@@ -19,36 +19,47 @@
 #define MAX_VALUE_LENGTH 32
 
 typedef enum { 
-    HOST_STATUS_USB,
-    HOST_STATUS_VND,
-    HOST_STATUS_PRD,
-    HOST_STATUS_BND,
-    HOST_STATUS_PPID,
-    HOST_STATUS_DEVID,
-    HOST_STATUS_FMT,
-    HOST_STATUS_IDX,
-    HOST_STATUS_TRT,
-    HOST_STATUS_ACK,
-    HOST_STATUS_PPC,
-    HOST_STATUS_TXID,
-    HOST_STATUS_RXID,
-    HOST_STATUS_RUN,
-    HOST_STATUS_CNT,
-    NUM_HOST_STATUS
-} ZING_HOST_STATUS_NAME;
+    DEVICE_STATUS_USB,
+    DEVICE_STATUS_PPID,
+    DEVICE_STATUS_DEVID,
+    DEVICE_STATUS_FMT,
+    DEVICE_STATUS_IDX,
+    DEVICE_STATUS_FPS,
+    DEVICE_STATUS_TRT,
+    DEVICE_STATUS_ACK,
+    DEVICE_STATUS_PPC,
+    DEVICE_STATUS_RUN,
+    DEVICE_STATUS_ITF,
+    DEVICE_STATUS_TXID,
+    DEVICE_STATUS_RXID,
+    DEVICE_STATUS_DST_ID_ERR_CNT,
+    DEVICE_STATUS_PHY_RX_FRAME_CNT,
+    DEVICE_STATUS_MFIR,
+    DEVICE_STATUS_CNT,
+    NUM_DEVICE_STATUS
+} ZING_DEVICE_STATUS_NAME;
 
 typedef struct {
-    int usb;            // USB value, e.g., "2"
-    char bnd;           // BND value, e.g., "L"
-    unsigned int ppid;  // PPID value, e.g.,"0xABCD"
-    unsigned int devid; // DeviceID, e.g.,  "0x3500"
-    char trt;           // TRT value, e.g., "B"
-    char ack;           // ACK value, e.g., "N"
-    char ppc;           // PPC value, e.g., "P"
-    unsigned int txid;  // TXID value, e.g.,"0x0"
-    unsigned int rxid;  // RXID value, e.g.,"0x0"
-    char run;           // RUN value, e.g., "N"
-    unsigned int cnt;   // CNT value, e.g., "0"
+    int usb;                // USB value, e.g., "2"
+    unsigned int ppid;      // PPID value, e.g., "0xABCD"
+    unsigned int devid;     // DeviceID, e.g., "0x3500"
+    int fmt;                // FMT value, e.g., "3"
+    int idx;                // IDX value, e.g., "3"
+    unsigned int fps;       // FPS value, e.g., "0x51615"
+    char trt;               // TRT value, e.g., "B"
+    char ack;               // ACK value, e.g., "N"
+    char ppc;               // PPC value, e.g., "D"
+    char run;               // RUN value, e.g., "Y"
+    char itf;               // ITF value, e.g., "N"
+    unsigned int txid;      // TXID value, e.g., "0x0"
+    unsigned int rxid;      // RXID value, e.g., "0x35ABCD"
+    int dest_err_cnt;       // DestID_ERR_CNT value before parentheses, e.g., "3"
+    int dest_err_sub;       // DestID_ERR_CNT value inside parentheses, e.g., "0"
+    int phy_rx_frame_cnt;   // PHY_RX_FRAME_CNT before parentheses, e.g., "1534345"
+    int phy_rx_sub;         // PHY_RX_FRAME_CNT inside parentheses, e.g., "232"
+    int mfir_main;          // MFIR value before '/', e.g., "0"
+    int mfir_sub;           // MFIR value after '/', e.g., "232"
+    unsigned int cnt;       // CNT value, e.g., "30899"
 } ZING_Data;
 
 // Define the type for the callback function
@@ -63,8 +74,8 @@ void RegisterEventCallback(EventCallback callback) {
 }
 
 void OnDataReceived(ZING_Data* z) {
-    sprintf(msg, "ZED USB:%d BND:%c PPID:0x%X DeviceID:0x%X TRT:%c ACK:%c PPC:%c TXID:0x%X RXID:0x%X RUN:%c CNT:%d\r\n",
-        z->usb, z->bnd, z->ppid, z->devid, z->trt, z->ack, z->ppc, z->txid, z->rxid, z->run, z->cnt);
+    sprintf(msg, "ZCD USB:%d PPID:0x%X DeviceID:0x%X FMT:%d IDX:%d FPS:0x%X TRT:%c ACK:%c PPC:%c RUN:%c ITF:%c TXID:0x%X RXID:0x%X DestID_ERR_CNT:%d(%d) PHY_RX_FRAME_CNT:%d(%d) MFIR:%d/%d CNT:%u\r\n",
+        z->usb, z->ppid, z->devid, z->fmt, z->idx, z->fps, z->trt, z->ack, z->ppc, z->run, z->itf, z->txid, z->rxid, z->dest_err_cnt, z->dest_err_sub, z->phy_rx_frame_cnt, z->phy_rx_sub, z->mfir_main, z->mfir_sub, z->cnt);
     UART_DBG_UartPutString(msg);
 }
 
@@ -87,18 +98,27 @@ CY_ISR(UART_ZING_RX_INTERRUPT)
 
             // Parsing the values into the structure
             if (sscanf(zing_status, 
-                       "ZED USB:%d BND:%c PPID:0x%X DeviceID:0x%X TRT:%c ACK:%c PPC:%c TXID:0x%X RXID:0x%X RUN:%c CNT:%d",
+                       "ZCD USB:%d PPID:0x%X DeviceID:0x%X FMT:%d IDX:%d FPS:0x%X TRT:%c ACK:%c PPC:%c RUN:%c ITF:%c TXID:0x%X RXID:0x%X DestID_ERR_CNT:%d(%d) PHY_RX_FRAME_CNT:%d(%d) MFIR:%d/%d CNT:%u",
                        &zing_data.usb,
-                       &zing_data.bnd,
                        &zing_data.ppid,
                        &zing_data.devid,
+                       &zing_data.fmt,
+                       &zing_data.idx,
+                       &zing_data.fps,
                        &zing_data.trt,
                        &zing_data.ack,
                        &zing_data.ppc,
+                       &zing_data.run,
+                       &zing_data.itf,
                        &zing_data.txid,
                        &zing_data.rxid,
-                       &zing_data.run,
-                       &zing_data.cnt) != 11) {
+                       &zing_data.dest_err_cnt,
+                       &zing_data.dest_err_sub,
+                       &zing_data.phy_rx_frame_cnt,
+                       &zing_data.phy_rx_sub,
+                       &zing_data.mfir_main,
+                       &zing_data.mfir_sub,
+                       &zing_data.cnt) != 20) {
                 UART_DBG_UartPutString("Parsing Error\r\n");
             } else {
                 if (event_callback != NULL) event_callback(&zing_data);
