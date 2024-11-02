83e061a89c71b2df02ecd46ee2c619559184f61c
Rossi Kwak <rossi.kwak@gmail.com>
2024-11-02 21:49:29 +0900

    인자의 변수 이름을 명확하게 하기 위해 imu로 변경

--- examples/examples/common/ImuFrame.h
+++ examples/examples/common/ImuFrame.h
@@ -14,29 +14,29 @@ typedef struct {
     bool isFull;
 }ImuFrame;
 
-void ImuFrame_init(ImuFrame *im)
+void ImuFrame_init(ImuFrame *imu)
 {
-    memset(im->data,0,IMU_FRAME_SIZE);
-    im->index = -1;
-    im->isEmpty = true;
-    im->isFull = false;
+    memset(imu->data,0,IMU_FRAME_SIZE);
+    imu->index = -1;
+    imu->isEmpty = true;
+    imu->isFull = false;
 }
 
-uint16_t ImuFrame_checksum(ImuFrame *im)
+uint16_t ImuFrame_checksum(ImuFrame *imu)
 {
     uint16_t checksum = 0;
-    for (uint8_t i = 0; i < (IMU_FRAME_SIZE-2); i++) checksum += (im->data[i]);
+    for (uint8_t i = 0; i < (IMU_FRAME_SIZE-2); i++) checksum += (imu->data[i]);
     return checksum;
 }
 
-int ImuFrame_integrity(ImuFrame *im)
+int ImuFrame_integrity(ImuFrame *imu)
 {
-    uint16_t checksum = ImuFrame_checksum(im);
+    uint16_t checksum = ImuFrame_checksum(imu);
     uint8_t high = (checksum&0xff00)>>8;
     uint8_t low = checksum&0xff;
 
-    if(im->data[IMU_FRAME_SIZE-2]!=high) return 0;
-    if(im->data[IMU_FRAME_SIZE-1]!=low) return 0;
+    if(imu->data[IMU_FRAME_SIZE-2]!=high) return 0;
+    if(imu->data[IMU_FRAME_SIZE-1]!=low) return 0;
     return 1;
 }
 
