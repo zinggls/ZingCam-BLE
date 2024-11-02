faccb758dbe5ae2e620e227c268a436522196a08
Rossi Kwak <rossi.kwak@gmail.com>
2024-11-02 18:24:42 +0900

    SOP와 checksum을 검사하여 정상적인 데이터들의 hex값을 문자열로 출력
    실행 결과
    Start
    55 55 FF E3 0 75 C7 B4 2B BD 5 64
    55 55 FF E3 0 75 C7 B4 2B D1 5 78
    55 55 FF E3 0 75 C7 B4 2B E5 5 8C
    55 55 FF E3 0 75 C7 B4 2B F9 5 A0
    55 55 FF E3 0 75 C7 B4 2C D 4 B5
    55 55 FF E3 0 75 C7 B4 2C 21 4 C9

--- examples/examples/imuNew.cydsn/main.c
+++ examples/examples/imuNew.cydsn/main.c
@@ -13,6 +13,8 @@
 #include <stdio.h>
 #include <UartBuf.h>
 
+#define IMU_FRAME_SIZE 12
+
 static UartBuf uBuf;    //Circular buffer for UART data
 
 CY_ISR(UART_IMU_RX_INTERRUPT)
@@ -25,13 +27,74 @@ CY_ISR(UART_IMU_RX_INTERRUPT)
 
 static char msg[128];
 
+static char imu[IMU_FRAME_SIZE];
+static int imuIndex = -1;
+static bool imuEmpty = true;
+static bool imuFull = false;
+
+uint16_t calculate_checksum(char *data, int length) {
+    uint16_t checksum = 0;
+    for (int i = 0; i < length; i++) {
+        checksum += data[i];
+    }
+    return checksum;
+}
+
 // Function to process data when a complete message is available
 static void process_uart_data()
 {
     while (!UartBuf_is_empty(&uBuf)) {
         char ch = UartBuf_read_char(&uBuf);
-        sprintf(msg, "%X ", ch);
-        UART_DBG_UartPutString(msg);
+        if(imuEmpty) {
+            if(ch!=0x55) continue;  //imu must start with 0x55
+            
+            imu[++imuIndex] = 0x55; //index=0
+            imuEmpty = false;
+        }else{
+            if(imuIndex==0) {
+                if(ch==0x55) {
+                    imu[++imuIndex] = 0x55; //index=1
+                }else{
+                    //imu 2nd byte must be 0x55, reset
+                    imuIndex = -1;
+                    imuEmpty = true;
+                    imuFull = false;
+                    continue;
+                }
+            }else if(imuIndex>0 && imuIndex<11){
+                imu[++imuIndex] = ch;
+            }else{
+                CYASSERT(imuIndex==(IMU_FRAME_SIZE-1));
+                
+                uint16_t checksum = calculate_checksum(imu,IMU_FRAME_SIZE-2);
+                uint8_t high = (checksum&0xff00)>>8;
+                uint8_t low = checksum&0xff;
+                
+                if(imu[IMU_FRAME_SIZE-2]==high && imu[IMU_FRAME_SIZE-1]==low) {
+                    //valid checksum
+                    imuFull = true;
+                }else{
+                    //invalid checksum
+                    imuIndex = -1;
+                    imuEmpty = true;
+                    imuFull = false;
+                    continue;
+                }
+            }
+        }
+        
+        if(imuFull) {
+            for(int i=0;i<IMU_FRAME_SIZE;i++) {
+                sprintf(msg, "%X ", imu[i]);
+                UART_DBG_UartPutString(msg);
+            }
+            sprintf(msg, "\r\n");
+            UART_DBG_UartPutString(msg);
+
+            imuIndex = -1;
+            imuEmpty = true;
+            imuFull = false;
+        }
     }
 }
 
