fdd1a7b3d35ce9cd407a3079994dc0ae899180be
Rossi Kwak <rossi.kwak@gmail.com>
2024-11-04 15:48:53 +0900

    ble-peripheral프로젝트를 복사하여 ble-central 프로젝트를 생성 ble컴포넌트 속성을 central client로 변경 비어있는 ble_callback,ble_events함수 생성
    실행 로그
    Start

---
+++ examples/examples/ble-central.cydsn/main.c
@@ -0,0 +1,44 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include <stdio.h>
+
+static char msg[128];
+
+void ble_callback(uint32 evt, void* param);
+void ble_events(uint32 evt, void* param);
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    CyBle_Start(ble_callback);
+    UART_DBG_Start();
+
+    UART_DBG_UartPutString("Start\r\n");
+    for(;;)
+    {
+        /* Place your application code here. */
+        CyBle_ProcessEvents();
+    }
+}
+
+void ble_callback(uint32 evt, void* param)
+{
+}
+
+void ble_events(uint32 evt, void* param)
+{
+}
+
+/* [] END OF FILE */
