3687092be07826fa6cfe2d342dce410a6b34a645
Rossi Kwak <rossi.kwak@gmail.com>
2024-11-04 15:40:51 +0900

    ble_events 케이스 정의
    실행결과
    Start
    BLE state = disconnected
    CYBLE_EVT_STACK_ON

--- examples/examples/ble-peripheral.cydsn/main.c
+++ examples/examples/ble-peripheral.cydsn/main.c
@@ -63,6 +63,53 @@ void ble_callback(uint32 evt, void* param)
 
 void ble_events(uint32 evt, void* param)
 {
+    switch (evt)
+    {
+        // callback when stack is available
+        case CYBLE_EVT_STACK_ON:
+            UART_DBG_UartPutString("CYBLE_EVT_STACK_ON\r\n");
+            break;
+        // callback when ble is disconnected
+        case CYBLE_EVT_GAP_DEVICE_DISCONNECTED:
+            UART_DBG_UartPutString("CYBLE_EVT_GAP_DEVICE_DISCONNECTED\r\n");
+            break;
+        // callback when ble is connected in application layer
+        case CYBLE_EVT_GATT_CONNECT_IND:
+            UART_DBG_UartPutString("CYBLE_EVT_GATT_CONNECT_IND\r\n");
+            break;
+        // callback when ble is connected in link layer
+        case CYBLE_EVT_GAP_ENHANCE_CONN_COMPLETE:
+            UART_DBG_UartPutString("CYBLE_EVT_GAP_ENHANCE_CONN_COMPLETE\r\n");
+            break;
+        // callback when ble is authenticated in link layer
+        case CYBLE_EVT_GAP_AUTH_COMPLETE:
+            UART_DBG_UartPutString("CYBLE_EVT_GAP_AUTH_COMPLETE\r\n");
+            break;
+        // callback when ble is discovered device
+        case CYBLE_EVT_GATTC_DISCOVERY_COMPLETE:
+            UART_DBG_UartPutString("CYBLE_EVT_GATTC_DISCOVERY_COMPLETE\r\n");
+            break;
+        // callback when receive mtu request
+        case CYBLE_EVT_GATTS_XCNHG_MTU_REQ:
+            UART_DBG_UartPutString("CYBLE_EVT_GATTS_XCNHG_MTU_REQ\r\n");
+            break;
+        // callback when receive mtu response
+        case CYBLE_EVT_GATTC_XCHNG_MTU_RSP:
+            UART_DBG_UartPutString("CYBLE_EVT_GATTC_XCHNG_MTU_RSP\r\n");
+            break;
+        // callback when receive write request
+        case CYBLE_EVT_GATTS_WRITE_REQ:
+            UART_DBG_UartPutString("CYBLE_EVT_GATTS_WRITE_REQ\r\n");
+            break;
+        // callback when receive write response
+        case CYBLE_EVT_GATTC_WRITE_RSP:
+            UART_DBG_UartPutString("CYBLE_EVT_GATTC_WRITE_RSP\r\n");
+            break;
+        // callback when receive notification
+        case CYBLE_EVT_GATTC_HANDLE_VALUE_NTF:
+            UART_DBG_UartPutString("CYBLE_EVT_GATTC_HANDLE_VALUE_NTF\r\n");
+            break;
+    }
 }
 
 /* [] END OF FILE */
