b3ec7bc3fb797b720fc0c7b6b22912b4624c7212
Rossi Kwak <rossi.kwak@gmail.com>
2024-11-13 17:40:42 +0900

    로그 보강

--- examples/examples/capsenseled.cydsn/main.c
+++ examples/examples/capsenseled.cydsn/main.c
@@ -1,4 +1,7 @@
 #include <project.h>
+#include <stdio.h>
+
+static char msg[128];
 
 uint16 fingerPos    = 0xFFFF;
 uint16 fingerPosOld = 0xFFFF;
@@ -21,6 +24,9 @@ void updateLed()
   	tempHandle.value.val = (uint8 *) &red_State;
     tempHandle.value.len = 1;
     CyBle_GattsWriteAttributeValue(&tempHandle,0,&cyBle_connHandle,CYBLE_GATT_DB_LOCALLY_INITIATED);  
+    
+    sprintf(msg,"updateLed, CyBle_GattsWriteAttributeValue, red state=%d\r\n",red_State);
+    UART_UartPutString(msg);
 }
 
 /***************************************************************
@@ -39,9 +45,13 @@ void updateCapsense()
     CyBle_GattsWriteAttributeValue(&tempHandle,0,&cyBle_connHandle,CYBLE_GATT_DB_LOCALLY_INITIATED );  
     
     /* send notification to client if notifications are enabled and finger location has changed */
-    if (capsenseNotify && (fingerPos != fingerPosOld) )
+    if (capsenseNotify && (fingerPos != fingerPosOld) ) {
         CyBle_GattsNotification(cyBle_connHandle,&tempHandle);
-        fingerPosOld = fingerPos;
+        sprintf(msg,"updateCapsense, CyBle_GattsNotification, pos=%d\r\n",fingerPos);
+        UART_UartPutString(msg);
+    }
+    
+    fingerPosOld = fingerPos;
 }
 
 /***************************************************************
@@ -55,14 +65,17 @@ void BleCallBack(uint32 event, void* eventParam)
     {
         /* if there is a disconnect or the stack just turned on from a reset then start the advertising and turn on the LED blinking */
         case CYBLE_EVT_STACK_ON:
+        UART_UartPutString("CYBLE_EVT_STACK_ON\r\n");
         case CYBLE_EVT_GAP_DEVICE_DISCONNECTED:
             capsenseNotify = 0;
             CyBle_GappStartAdvertisement(CYBLE_ADVERTISING_FAST);
             pwm_Start();
+            UART_UartPutString("Advertising fast\r\n");
             break;
         
         /* when a connection is made, update the LED and Capsense states in the GATT database and stop blinking the LED */    
         case CYBLE_EVT_GATT_CONNECT_IND:
+            UART_UartPutString("CYBLE_EVT_GATT_CONNECT_IND\r\n");
             updateLed();
             updateCapsense();  
             pwm_Stop();
@@ -70,29 +83,41 @@ void BleCallBack(uint32 event, void* eventParam)
 
         /* handle a write request */
         case CYBLE_EVT_GATTS_WRITE_REQ:
+            UART_UartPutString("CYBLE_EVT_GATTS_WRITE_REQ");
             wrReqParam = (CYBLE_GATTS_WRITE_REQ_PARAM_T *) eventParam;
 			      
             /* request write the LED value */
             if(wrReqParam->handleValPair.attrHandle == CYBLE_LEDCAPSENSE_LED_CHAR_HANDLE)
             {
+                UART_UartPutString(",request write LED value");
+                
                 /* only update the value and write the response if the requested write is allowed */
                 if(CYBLE_GATT_ERR_NONE == CyBle_GattsWriteAttributeValue(&wrReqParam->handleValPair, 0, &cyBle_connHandle, CYBLE_GATT_DB_PEER_INITIATED))
                 {
                     red_Write(!wrReqParam->handleValPair.value.val[0]);
                     CyBle_GattsWriteRsp(cyBle_connHandle);
+                    UART_UartPutString(",CyBle_GattsWriteRsp");
                 }
             }
             
             /* request to update the CapSense notification */
             if(wrReqParam->handleValPair.attrHandle == CYBLE_LEDCAPSENSE_CAPSENSE_CAPSENSECCCD_DESC_HANDLE) 
             {
+                UART_UartPutString(",request update CapSense notification");
+                
                 CyBle_GattsWriteAttributeValue(&wrReqParam->handleValPair, 0, &cyBle_connHandle, CYBLE_GATT_DB_PEER_INITIATED);
                 capsenseNotify = wrReqParam->handleValPair.value.val[0] & 0x01;
                 CyBle_GattsWriteRsp(cyBle_connHandle);
-            }		
+                
+                sprintf(msg,",capsenseNotity=%d",capsenseNotify);
+                UART_UartPutString(msg);
+            }
+            UART_UartPutString("\r\n");
 			break;  
         
         default:
+            sprintf(msg,"unhandled BLE event = 0x%lx\r\n",event);
+            UART_UartPutString(msg);
             break;
     }
 } 
