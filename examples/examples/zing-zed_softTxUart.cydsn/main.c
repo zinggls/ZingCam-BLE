f42eb3240843cd7f9fd7bff0c7d9810d2a7e0fd3
Rossi Kwak <rossi.kwak@gmail.com>
2024-11-25 21:58:22 +0900

    zing-zed_softTxUart 프로젝트 추가

---
+++ examples/examples/zing-zed_softTxUart.cydsn/main.c
@@ -0,0 +1,138 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <UartBuf.h>
+
+#define ASCII_HOST 'Z'
+#define ASCII_DEVICE 'Z'
+#define ASCII_LF '\n'
+#define MAX_VALUE_LENGTH 32
+
+typedef enum { 
+    HOST_STATUS_USB,
+    HOST_STATUS_VND,
+    HOST_STATUS_PRD,
+    HOST_STATUS_BND,
+    HOST_STATUS_PPID,
+    HOST_STATUS_DEVID,
+    HOST_STATUS_FMT,
+    HOST_STATUS_IDX,
+    HOST_STATUS_TRT,
+    HOST_STATUS_ACK,
+    HOST_STATUS_PPC,
+    HOST_STATUS_TXID,
+    HOST_STATUS_RXID,
+    HOST_STATUS_RUN,
+    HOST_STATUS_CNT,
+    NUM_HOST_STATUS
+} ZING_HOST_STATUS_NAME;
+
+typedef struct {
+    int usb;            // USB value, e.g., "2"
+    char bnd;           // BND value, e.g., "L"
+    unsigned int ppid;  // PPID value, e.g.,"0xABCD"
+    unsigned int devid; // DeviceID, e.g.,  "0x3500"
+    char trt;           // TRT value, e.g., "B"
+    char ack;           // ACK value, e.g., "N"
+    char ppc;           // PPC value, e.g., "P"
+    unsigned int txid;  // TXID value, e.g.,"0x0"
+    unsigned int rxid;  // RXID value, e.g.,"0x0"
+    char run;           // RUN value, e.g., "N"
+    unsigned int cnt;   // CNT value, e.g., "0"
+} ZING_Data;
+
+static char msg[256];
+static UartBuf uBuf;    //Circular buffer for UART data
+static ZING_Data zing_data;
+
+CY_ISR(UART_ZING_RX_INTERRUPT)
+{
+    char ch = UART_ZING_GetChar();
+    if (ch != 0) {
+        UartBuf_write_char(&uBuf,ch);  // Write character to circular buffer
+
+        // Check for end of message
+        if (ch == ASCII_LF) {
+            uBuf.message_complete = true;
+        }
+    }
+
+    // Clear the interrupt to prevent retriggering
+    UART_ZING_RX_ClearInterrupt();
+}
+
+static void PrintZingData(ZING_Data* z) {
+    sprintf(msg, "ZED USB:%d BND:%c PPID:0x%X DeviceID:0x%X TRT:%c ACK:%c PPC:%c TXID:0x%X RXID:0x%X RUN:%c CNT:%d\r\n",
+        z->usb, z->bnd, z->ppid, z->devid, z->trt, z->ack, z->ppc, z->txid, z->rxid, z->run, z->cnt);
+    SW_Tx_UART_PutString(msg);
+}
+
+// Function to process data when a complete message is available
+static void process_uart_data()
+{
+    if (uBuf.message_complete) {
+        // Extract complete message from buffer
+        char zing_status[MAX_BUFFER_LENGTH] = {0};
+        uint16_t cnt = 0;
+        
+        while (!UartBuf_is_empty(&uBuf)) {
+            zing_status[cnt++] = UartBuf_read_char(&uBuf);
+        }
+        
+        zing_status[cnt] = '\0';  // Null-terminate the string
+        uBuf.message_complete = false;
+
+        // Parsing the values into the structure
+        if (sscanf(zing_status, 
+                   "ZED USB:%d BND:%c PPID:0x%X DeviceID:0x%X TRT:%c ACK:%c PPC:%c TXID:0x%X RXID:0x%X RUN:%c CNT:%d",
+                   &zing_data.usb,
+                   &zing_data.bnd,
+                   &zing_data.ppid,
+                   &zing_data.devid,
+                   &zing_data.trt,
+                   &zing_data.ack,
+                   &zing_data.ppc,
+                   &zing_data.txid,
+                   &zing_data.rxid,
+                   &zing_data.run,
+                   &zing_data.cnt) != 11) {
+            SW_Tx_UART_PutString("Parsing Error\r\n");
+            SW_Tx_UART_PutString("Received: ");
+            SW_Tx_UART_PutString(zing_status);
+            SW_Tx_UART_PutString("\r\n");
+        } else {
+            PrintZingData(&zing_data);
+        }
+    }
+}
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    SW_Tx_UART_Start();
+    UART_ZING_Start();
+    UartBuf_init(&uBuf);
+    UART_ZING_RX_INTR_StartEx(UART_ZING_RX_INTERRUPT);
+    
+    SW_Tx_UART_PutString("Start\r\n");
+    for(;;)
+    {
+        /* Place your application code here. */
+        process_uart_data();
+    }
+}
+
+/* [] END OF FILE */
