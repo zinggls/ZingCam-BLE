45fabc054487e0fb12015785c354703ff74aa5ab
Rossi Kwak <rossi.kwak@gmail.com>
2024-11-27 00:43:56 +0900

    COM Object를 사용하는 Cypress 예제 프로젝트 Cpp_Ex와 이를 참고하여 COM초기화만 진행해본 comOpen프로젝트 추가

---
+++ examples/examples/I2C_SCB_Slave.cydsn/i2cMaster/comOpen/comOpen.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	HRESULT hr = 0;
+	cout << "Initializing COM" << endl;
+	if (FAILED(CoInitialize(NULL)))
+	{
+		cout << "Unable to initialize COM" << endl;
+		return 0;
+	}
+	cout << "COM Initialized" << endl;
+
+	cout << "Shutting down COM" << endl;
+	CoUninitialize();
+	return 0;
+}
