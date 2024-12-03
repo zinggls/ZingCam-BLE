c1b868536236c8c9997bde279989797ae42378f1
Rossi Kwak <rossi.kwak@gmail.com>
2024-12-03 12:30:00 +0900

    indentation 수정

--- examples/examples/zi-bcc.cydsn/bcc.c
+++ examples/examples/zi-bcc.cydsn/bcc.c
@@ -23,31 +23,31 @@ ZCD_FRAME* getZcdFrame() { return &zcdFrame; }
 
 static void processingZxx()
 {
-            if(*getZxxKind()==Unknown) *getZxxKind() = inspect((char*)notificationParam->handleValPair.value.val);
-            
-            if(notificationParam->handleValPair.value.len == getFrameSize()) {
-                notifiedCustom++;
-                
-                // Process the received data                                
-                if(*getZxxKind()==ZED) {
-                    memcpy(&zedFrame,notificationParam->handleValPair.value.val,notificationParam->handleValPair.value.len);
-                    setZedBuffer(getI2CReadBuffer()+46,&zedFrame);
-                    setZedImuBuffer(getI2CReadBuffer()+22,&zedFrame);   //ICD 무선영상 송신기 IMU
-                    getZcdFrame()->pos = zedFrame.pos;
-                    LED_RED_Write  (0);
-                    LED_GREEN_Write(1);
-                    LED_BLUE_Write (0);
-                }
-                if(*getZxxKind()==ZCH) {
-                    memcpy(&zchFrame,notificationParam->handleValPair.value.val,notificationParam->handleValPair.value.len);
-                    setZchBuffer(getI2CReadBuffer()+46,&zchFrame);
-                    setZchImuBuffer(getI2CReadBuffer()+22,&zchFrame);   //ICD 무선영상 송신기 IMU
-                    getZcdFrame()->pos = zchFrame.pos;
-                    LED_RED_Write  (0);
-                    LED_GREEN_Write(0);
-                    LED_BLUE_Write (1);
-                }
-            }
+    if(*getZxxKind()==Unknown) *getZxxKind() = inspect((char*)notificationParam->handleValPair.value.val);
+    
+    if(notificationParam->handleValPair.value.len == getFrameSize()) {
+        notifiedCustom++;
+        
+        // Process the received data                                
+        if(*getZxxKind()==ZED) {
+            memcpy(&zedFrame,notificationParam->handleValPair.value.val,notificationParam->handleValPair.value.len);
+            setZedBuffer(getI2CReadBuffer()+46,&zedFrame);
+            setZedImuBuffer(getI2CReadBuffer()+22,&zedFrame);   //ICD 무선영상 송신기 IMU
+            getZcdFrame()->pos = zedFrame.pos;
+            LED_RED_Write  (0);
+            LED_GREEN_Write(1);
+            LED_BLUE_Write (0);
+        }
+        if(*getZxxKind()==ZCH) {
+            memcpy(&zchFrame,notificationParam->handleValPair.value.val,notificationParam->handleValPair.value.len);
+            setZchBuffer(getI2CReadBuffer()+46,&zchFrame);
+            setZchImuBuffer(getI2CReadBuffer()+22,&zchFrame);   //ICD 무선영상 송신기 IMU
+            getZcdFrame()->pos = zchFrame.pos;
+            LED_RED_Write  (0);
+            LED_GREEN_Write(0);
+            LED_BLUE_Write (1);
+        }
+    }
 }
 
 /* BLE App Callback Function */
