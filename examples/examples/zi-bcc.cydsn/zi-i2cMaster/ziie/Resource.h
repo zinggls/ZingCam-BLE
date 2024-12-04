652803a5389aded03481be39ac0cf080db4671d2
Rossi Kwak <rossi.kwak@gmail.com>
2024-12-04 09:00:43 +0900

    GLS로고 추가

--- examples/examples/zi-bcc.cydsn/zi-i2cMaster/ziie/Resource.h
+++ examples/examples/zi-bcc.cydsn/zi-i2cMaster/ziie/Resource.h
@@ -7,6 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_ZIIE_DIALOG                 102
 #define IDR_MAINFRAME                   128
+#define IDB_GLS_BITMAP                  130
 #define IDC_LOG_LIST                    1000
 #define IDC_I2C_READ_BUTTON             1001
 #define IDC_LIST1                       1002
@@ -56,14 +57,15 @@
 #define IDC_LIST2                       1042
 #define IDC_WRITE_BUFFER_LIST           1042
 #define IDC_I2C_WRITE_BUTTON            1043
+#define IDC_LOGO_STATIC                 1044
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1043
+#define _APS_NEXT_CONTROL_VALUE         1045
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
