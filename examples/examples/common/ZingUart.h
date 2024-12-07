ea3c0ddd5bef056e0586d4e32c893cb3786f144a
Rossi Kwak <rossi.kwak@gmail.com>
2024-12-08 07:22:19 +0900

    ZingUart.h를 헤더와 구현부(ZingUart.c)로 분리

--- examples/examples/common/ZingUart.h
+++ examples/examples/common/ZingUart.h
@@ -6,50 +6,10 @@
     
 #define ASCII_LF '\n'
     
-static UartBuf uBuf;    //Circular buffer for UART data
 typedef void (*ZingRxCallback)(const char *buf);
-ZingRxCallback zingRxCb = NULL;
 
-void ZingUart_Init(ZingRxCallback cb)
-{
-    UartBuf_init(&uBuf);
-    zingRxCb = cb;
-}
-    
-CY_ISR(UART_ZING_RX_INTERRUPT)
-{
-    char ch = UART_ZING_GetChar();
-    if (ch != 0) {
-        UartBuf_write_char(&uBuf,ch);  // Write character to circular buffer
-
-        // Check for end of message
-        if (ch == ASCII_LF) {
-            uBuf.message_complete = true;
-        }
-    }
-
-    // Clear the interrupt to prevent retriggering
-    UART_ZING_RX_ClearInterrupt();
-}
-
-// Function to process data when a complete message is available
-void zing_process_uart_data()
-{
-    if (uBuf.message_complete) {
-        // Extract complete message from buffer        
-        char zing_status[MAX_BUFFER_LENGTH] = {0};
-        uint16_t cnt = 0;
-        
-        while (!UartBuf_is_empty(&uBuf)) {
-            zing_status[cnt++] = UartBuf_read_char(&uBuf);
-        }
-        
-        zing_status[cnt] = '\0';  // Null-terminate the string
-        uBuf.message_complete = false;
-        
-        CYASSERT(zingRxCb);
-        zingRxCb(zing_status);
-    }
-}
+void ZingUart_Init(ZingRxCallback cb);
+CY_ISR(UART_ZING_RX_INTERRUPT);
+void zing_process_uart_data();
 
 #endif /* ZING_UART_H */
