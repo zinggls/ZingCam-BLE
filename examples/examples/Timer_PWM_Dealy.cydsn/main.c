3ea2426e5e427bd00cf58de5ba90d7f0e8ba1ef1
Rossi Kwak <rossi.kwak@gmail.com>
2024-12-10 07:39:11 +0900

    Timer_PWM 프로젝트를 복사해서 Timer_PWM_Delay 생성

---
+++ examples/examples/Timer_PWM_Dealy.cydsn/main.c
@@ -0,0 +1,61 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+#define BRIGHTNESS_CHANGE         (200u)
+
+/******************************************************************************
+* Function Name: Isr_TC_Handler
+*******************************************************************************
+*
+* Summary:
+*  Handles the Interrupt Service Routine for the PWM Component.
+*
+******************************************************************************/
+CY_ISR(Isr_TC_Handler)
+{   
+	/* Clear the TC Interrupt */
+   	PWM_ClearInterrupt(PWM_INTR_MASK_TC);
+    
+    /* Decrement the PWM Compare value to increase the LED brightness */
+    PWM_WriteCompare(PWM_ReadCompare() - BRIGHTNESS_CHANGE);
+}
+
+
+/******************************************************************************
+* Function Name: main
+*******************************************************************************
+*
+* Summary:
+*  This function:
+*  1. Sets up and enables the PWM interrupt.
+*  2. Starts the PWM Component.
+*
+******************************************************************************/
+int main()
+{   
+    /* Enable the global interrupt */
+    CyGlobalIntEnable;
+    
+    /* Enable the Interrupt component connected to the PWM interrupt output */
+    Isr_TC_StartEx(Isr_TC_Handler);
+    
+	/* Start the PWM Component */
+    PWM_Start();
+    
+    for(;;)
+    {
+        
+    }
+}
+
+/* [] END OF FILE */
