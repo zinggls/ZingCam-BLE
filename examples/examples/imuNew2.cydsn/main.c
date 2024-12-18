e743a8bedde3d0a7aa4ee44a74bee51ba31e7ca9
Rossi Kwak <rossi.kwak@gmail.com>
2024-12-18 09:11:39 +0900

    imuNew1 프로젝트를 복사해서 imuNew2를 만듬

---
+++ examples/examples/imuNew2.cydsn/main.c
@@ -0,0 +1,44 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include <stdio.h>
+#include "imu.h"
+
+static char msg[128];
+
+static void onImuFrame(const ImuFrame *imu)
+{
+    for(int i=0;i<IMU_FRAME_SIZE;i++) {
+        sprintf(msg, "%X ", imu->data[i]);
+        UART_DBG_UartPutString(msg);
+    }
+    sprintf(msg, "\r\n");
+    UART_DBG_UartPutString(msg);
+}
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */    
+    UART_DBG_Start();
+    UART_IMU_StartAndInitialize();
+    
+    UART_DBG_UartPutString("Start\r\n");
+    for(;;)
+    {
+        /* Place your application code here. */
+        imu_process_uart_data(onImuFrame);
+    }
+}
+
+/* [] END OF FILE */
