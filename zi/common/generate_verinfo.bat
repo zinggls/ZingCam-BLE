36dea86100fbec324015f9b6408a8e7be7f560cc
Rossi Kwak <rossi.kwak@gmail.com>
2025-01-04 21:53:38 +0900

    git describe 결과를 git_describe.h로 만들어 주는 스크립트 추가

--- zi/common/generate_verinfo.bat
+++ zi/common/generate_verinfo.bat
@@ -1,1 +1,19 @@
 git log --date=short --pretty=format:"#define VER_INFO_PRESENT %%nstatic const char* VER_INFO = \"%%h %%an %%ad\"; " -n 1 > version.h
+
+@echo off
+REM Batch file to generate a C header file from `git describe`
+
+REM Run git describe and store the output
+for /f "delims=" %%i in ('git describe') do set GIT_DESCRIBE=%%i
+
+REM Define the output header file name
+set HEADER_FILE=git_describe.h
+
+REM Generate the header file content
+echo #ifndef GIT_DESCRIBE_H > %HEADER_FILE%
+echo #define GIT_DESCRIBE_H >> %HEADER_FILE%
+echo. >> %HEADER_FILE%
+echo #define GIT_DESCRIBE_PRESENT >> %HEADER_FILE%
+echo static const char* GIT_DESCRIBE = "%GIT_DESCRIBE%"; >> %HEADER_FILE%
+echo. >> %HEADER_FILE%
+echo #endif /* GIT_DESCRIBE_H */ >> %HEADER_FILE%
