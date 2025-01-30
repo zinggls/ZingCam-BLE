f101f98f3ce9025725869a74a816111b6eb16a7b
Rossi Kwak <rossi.kwak@gmail.com>
2025-01-30 16:46:48 +0900

    의미별 실행 단위를 함수로 정의, 루프 시작시 Pioneer board의 RGB LED를 깜빡하도록 변경

--- zi/tests/FUB-002_BIB-001.cydsn/main.c
+++ zi/tests/FUB-002_BIB-001.cydsn/main.c
@@ -14,47 +14,73 @@
 #define ON  0x0
 #define OFF 0x1
 
+void TxRun_Test();
+void SPDT_Test();
+void CH_LED_Test();
+void Batt_state_LED_Test();
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    TX_Run_Write(OFF);
-    SPDT_Write(OFF);
-    CH_LED_Write(0x0);
-    Batt_state_LED_Write(0x7);
 
     for(;;)
     {
         /* Place your application code here. */
-        TX_Run_Write(~TX_Run_Read());
-        SPDT_Write(~SPDT_Read());
-        CyDelay(1000);
+        for(int i=0;i<3;i++)
+        {
+            SW_LED_LED_Write(~SW_LED_LED_Read());
+            CyDelay(100);
+        }
         
-        CH_LED_Write(0x1);
-        CyDelay(1000);
-        CH_LED_Write(0x2);
-        CyDelay(1000);
-        CH_LED_Write(0x3);
-        CyDelay(1000);
-
-        Batt_state_LED_Write(0x7);
-        CyDelay(1000);
-        Batt_state_LED_Write(0x6);
-        CyDelay(1000);
-        Batt_state_LED_Write(0x5);
-        CyDelay(1000);
-        Batt_state_LED_Write(0x4);
-        CyDelay(1000);
-        Batt_state_LED_Write(0x3);
-        CyDelay(1000);
-        Batt_state_LED_Write(0x2);
-        CyDelay(1000);
-        Batt_state_LED_Write(0x1);
-        CyDelay(1000);
-        Batt_state_LED_Write(0x0);
-        CyDelay(1000);
+        TxRun_Test();
+        SPDT_Test();
+        CH_LED_Test();
+        Batt_state_LED_Test();
     }
+}
+
+void TxRun_Test()
+{
+    TX_Run_Write(~TX_Run_Read());
+    CyDelay(1000);
+}
+
+void SPDT_Test()
+{
+    SPDT_Write(~SPDT_Read());
+    CyDelay(1000);
+}
+
+void CH_LED_Test()
+{
+    CH_LED_Write(0x1);
+    CyDelay(1000);
+    CH_LED_Write(0x2);
+    CyDelay(1000);
+    CH_LED_Write(0x3);
+    CyDelay(1000);
+}
+
+void Batt_state_LED_Test()
+{
+    Batt_state_LED_Write(0x7);
+    CyDelay(1000);
+    Batt_state_LED_Write(0x6);
+    CyDelay(1000);
+    Batt_state_LED_Write(0x5);
+    CyDelay(1000);
+    Batt_state_LED_Write(0x4);
+    CyDelay(1000);
+    Batt_state_LED_Write(0x3);
+    CyDelay(1000);
+    Batt_state_LED_Write(0x2);
+    CyDelay(1000);
+    Batt_state_LED_Write(0x1);
+    CyDelay(1000);
+    Batt_state_LED_Write(0x0);
+    CyDelay(1000);
 }
 
 /* [] END OF FILE */
