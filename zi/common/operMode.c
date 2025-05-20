387236bbbb74192cf5ca7f561f7764282366a605
Rossi Kwak <rossi.kwak@gmail.com>
2025-05-20 09:25:01 +0900

    ref 버전을 참고하여 onWirelessVideoTransmitterOperationMode, onWirelessVideoReceiverOperationMode 함수에 적용
    ref.
    operModeSimulation 9212d6345bbc0807e71e98eb4d15bb7259ec4ef6

--- zi/common/operMode.c
+++ zi/common/operMode.c
@@ -1,6 +1,7 @@
 #include "operMode.h"
 
-static uint8_t prevMode = MODE_OPER;
+static uint8_t txPrevMode = MODE_PSAVE;
+static uint8_t rxPrevMode = MODE_PSAVE;
 static ZingRxCallback zingRxCb = NULL;
 
 void setZingRxCallback(ZingRxCallback cb)
@@ -10,8 +11,109 @@ void setZingRxCallback(ZingRxCallback cb)
 
 void onWirelessVideoTransmitterOperationMode(uint8_t mode)
 {
+	if (txPrevMode == MODE_OPER || txPrevMode == MODE_DEFAULT) {        //운용모드
+		if (mode == MODE_WAIT) {
+			//운용모드->대기모드
+			HGATE_Con1_Write(0x04);
+			HGATE_Con1_Write(0x00);
+		}
+		else if (mode == MODE_PSAVE) {
+			//운용모드->절전모드
+			HGATE_Con1_Write(0x04);
+			HGATE_Con1_Write(0x00);
+			PW_EN_Write(0);
+		}
+
+		txPrevMode = mode;
+	}
+	else if (txPrevMode == MODE_WAIT) {    //대기모드
+		if (mode == MODE_OPER || mode == MODE_DEFAULT) {
+			//대기모드->운용모드
+			HGATE_Con1_Write(0x03);	//송신기 디폴트 출력
+		}
+		else if (mode == MODE_PSAVE) {
+			//대기모드->절전모드
+			PW_EN_Write(0);
+		}
+
+		txPrevMode = mode;
+	}
+	else if (txPrevMode == MODE_PSAVE) {    //절전모드
+		if (mode == MODE_OPER || mode == MODE_DEFAULT) {
+			//절전모드->운용모드
+			PW_EN_Write(1);
+			UartRestart(zingRxCb);
+
+			HGATE_Con1_Write(0x03);	//송신기 디폴트 출력
+		}
+		else if (mode == MODE_WAIT) {
+			//절전모드->대기모드
+			PW_EN_Write(1);
+			UartRestart(zingRxCb);
+		}
+
+		txPrevMode = mode;
+	}
 }
 
 void onWirelessVideoReceiverOperationMode(uint8_t mode)
 {
+	if (rxPrevMode == MODE_OPER || rxPrevMode == MODE_DEFAULT) {        //운용모드
+		if (mode == MODE_WAIT) {
+			//운용모드->대기모드
+			HGATE_Con1_Write(0x04);
+			HGATE_Con2_1_Write(0x04);
+			HGATE_Con2_2_Write(0x04);
+			HGATE_Con1_Write(0x00);
+			HGATE_Con2_1_Write(0x00);
+			HGATE_Con2_2_Write(0x00);
+		}
+		else if (mode == MODE_PSAVE) {
+			//운용모드->절전모드
+			HGATE_Con1_Write(0x04);
+			HGATE_Con2_1_Write(0x04);
+			HGATE_Con2_2_Write(0x04);
+			HGATE_Con1_Write(0x00);
+			HGATE_Con2_1_Write(0x00);
+			HGATE_Con2_2_Write(0x00);
+			PW_EN_Write(0);
+		}
+
+		rxPrevMode = mode;
+	}
+	else if (rxPrevMode == MODE_WAIT) {    //대기모드
+		if (mode == MODE_OPER || mode == MODE_DEFAULT) {
+			//대기모드->운용모드
+
+			//수신기 디폴트 출력
+			HGATE_Con1_Write(0x03);
+			HGATE_Con2_1_Write(0x00);
+			HGATE_Con2_2_Write(0x03);
+		}
+		else if (mode == MODE_PSAVE) {
+			//대기모드->절전모드
+			PW_EN_Write(0);
+		}
+
+		rxPrevMode = mode;
+	}
+	else if (rxPrevMode == MODE_PSAVE) {    //절전모드
+		if (mode == MODE_OPER || mode == MODE_DEFAULT) {
+			//절전모드->운용모드
+			PW_EN_Write(1);
+			UartRestart(zingRxCb);
+
+			//수신기 디폴트 출력
+			HGATE_Con1_Write(0x03);
+			HGATE_Con2_1_Write(0x00);
+			HGATE_Con2_2_Write(0x03);
+		}
+		else if (mode == MODE_WAIT) {
+			//절전모드->대기모드
+			PW_EN_Write(1);
+			UartRestart(zingRxCb);
+		}
+
+		rxPrevMode = mode;
+	}
 }
--- zi/common/operMode.h
+++ zi/common/operMode.h
@@ -13,6 +13,7 @@
     default(0x0) 모드는 운용모드와 동일, 펌웨어 내에서는 모드값 0은 존재하지 않고 운용(0x1),대기(0x2),절전(0x4)값만 정의된다
 */
 
+#define MODE_DEFAULT (0x00)
 #define MODE_OPER    (0x01)
 #define MODE_WAIT    (0x02)
 #define MODE_PSAVE   (0x04)
